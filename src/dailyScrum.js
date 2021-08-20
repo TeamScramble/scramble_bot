0cac401c4e52e5316fc72bf4bcfec0a0564c1786
warmwhiten <warmwhiten@gmail.com>
2021-08-21 01:25:56 +0900

    fix: hide discord webhook url in code

--- src/dailyScrum.js
+++ src/dailyScrum.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
+require('dotenv').config();
 
 
 exports.handler = async (event) => {
 
     try {
-        const result = await axios.post("https://discord.com/api/webhooks/878293255857524826/pN3oIaw_rzUpPzJuaXui8nbwPH_YTfQyckHjjZOttU9S0JGZYrkX9u86giNEojIn0S9g", {
+        const result = await axios.post(process.env.DAILYSCRUM_URL, {
             "content": "안뇽 :wave: 데일리 스크럼을 해주세요! \n :white_check_mark: 체크인: \n :white_check_mark: 해야할 일: \n :white_check_mark: 협업할 일: \n :white_check_mark: 논의할 일: "
         })
         console.info("디스코드 웹훅 전송 성공!")
